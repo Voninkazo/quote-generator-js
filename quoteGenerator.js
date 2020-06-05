e2e91801c33d4dd09a1c5aee3ccd32148f95b82c
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-06-05 09:07:37 +0300

    I generated the array randomly and create an new array for all of the quotes to be together

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -13,33 +13,19 @@ const quote3 = [
   'I know that man is capable of great deeds.',
   'But if he isn\'t capable of great emotion, well, he leaves me cold.'
 ]
-console.log(quote1, quote2, quote3);
 
-var quoteChoice = Number(prompt('Your number of quotes'));
-
-
-// Generate the array
+// Generate the array randomly
 function generateQuotes() {
-  var random = Math.floor(Math.random) * (myQuotes.length);
-  var currentQuote = myQuotes[random];
-  console.log(currentQuote);
+  var quoteRandom1 = Math.floor(Math.random) * (quote1.length);
+  var quoteRandom2 = Math.floor(Math.random) * (quote2.length);
+  var quoteRandom3 = Math.floor(Math.random) * (quote3.length);
 }
 
-  //Here I am using for loop to loop through the array and I use if else statement to set the conditions on how many qoutes the user wants
+//Here I am using for loop to loop through the array and I use if else statement to set the conditions on how many qoutes the user wants
 
-  for (let i = 0; i < 5; i++) {
-    if (quoteChoice === "0") {
-      return quote1;
-    } else if (quoteChoice === "2") {
-      return quote2;
-    } else if (quoteChoice === "3") {
-      return quote3;
-    } else {
-      undefined;
-    }
-    var currentQuote = "Here is the quote: " +
-      quote1[Math.floor(Math.random() * quote1.length)] + ' ' +
-      quote2[Math.floor(Math.random() * quote2.length)] + ' ' +
-      quote3[Math.floor(Math.random() * quote3.length)] + '!';
-    return currentQuote;
-  }
+for (let i = 0; i < quote1.length + quote2.length + quote3.length; i++) {
+  var randomQuote = quote1 + ` ` + quote2 + ` ` + ` ` + quote3;
+  var singleQuote = [];
+  singleQuote.push(randomQuote);
+}
+console.log(singleQuote);
