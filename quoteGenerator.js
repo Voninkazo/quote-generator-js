91e52036aa1eba772a9179fde12643d5fc821c97
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-06-05 08:32:07 +0300

    removed the first array conatins a lot of strings and created three new diffrent arrays

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,6 +1,22 @@
-// Create a variable that contains an array of quotes
-var myQuotes = ["You \'ve gotta dance like there\'s nobody watching.", " You\'re not getting away from me.", "Never again.", "Be the change that you wish to see in the world.", "I envy people that know love.", "That have someone who takes them as they are.", "No one can make you feel inferior without your consent.", "I know that man is capable of great deeds.", "But if he isn\'t capable of great emotion, well, he leaves me cold."];
-console.log(myQuotes);
+// Create three arrays of object.
+const quote1 = [
+  'You \'ve gotta dance like there\'s nobody watching.',
+  ' You\'re not getting away from me.',
+  'Never again.'
+]
+const quote2 = [
+  'Be the change that you wish to see in the world.',
+  'I envy people that know love.', 'That have someone who takes them as they are.'
+]
+const quote3 = [
+  'No one can make you feel inferior without your consent.',
+  'I know that man is capable of great deeds.',
+  'But if he isn\'t capable of great emotion, well, he leaves me cold.'
+]
+console.log(quote1, quote2, quote3);
+
+var quoteChoice = Number(prompt('Your number of quotes'));
+
 
 // Generate the array
 function generateQuotes() {
@@ -9,13 +25,6 @@ function generateQuotes() {
   console.log(currentQuote);
 }
 
-var quoteChoice = Number(prompt('Your number of quotes'));
-
-// Create a function to generate the quotes
-function generatingQuotes() {
-  var quote1 = ["You \'ve gotta dance like there\'s nobody watching.", " You\'re not getting away from me.", "Never again."]
-  var quote2 = ["Be the change that you wish to see in the world.", "I envy people that know love.", "That have someone who takes them as they are."]
-  var quote3 = ["No one can make you feel inferior without your consent.", "I know that man is capable of great deeds.", "But if he isn\'t capable of great emotion, well, he leaves me cold."]
   //Here I am using for loop to loop through the array and I use if else statement to set the conditions on how many qoutes the user wants
 
   for (let i = 0; i < 5; i++) {
@@ -34,4 +43,3 @@ function generatingQuotes() {
       quote3[Math.floor(Math.random() * quote3.length)] + '!';
     return currentQuote;
   }
-}
