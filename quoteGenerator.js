2269532229bf4d4cb358d699ba26a275028061ff
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-06-05 14:05:52 +0300

    I strted working to generate the array of quote

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -6,7 +6,8 @@ const quote1 = [
 ]
 const quote2 = [
   'Be the change that you wish to see in the world.',
-  'I envy people that know love.', 'That have someone who takes them as they are.'
+  'I envy people that know love.',
+  'That have someone who takes them as they are.'
 ]
 const quote3 = [
   'No one can make you feel inferior without your consent.',
@@ -14,18 +15,29 @@ const quote3 = [
   'But if he isn\'t capable of great emotion, well, he leaves me cold.'
 ]
 
-// Generate the array randomly
-function generateQuotes() {
-  var quoteRandom1 = Math.floor(Math.random) * (quote1.length);
-  var quoteRandom2 = Math.floor(Math.random) * (quote2.length);
-  var quoteRandom3 = Math.floor(Math.random) * (quote3.length);
-}
+// Generate the array randomly 
+let quoteRandom1 = Math.floor(Math.random) * (quote1.length);
+let quoteRandom2 = Math.floor(Math.random) * (quote2.length);
+let quoteRandom3 = Math.floor(Math.random) * (quote3.length);
 
-//Here I am using for loop to loop through the array and I use if else statement to set the conditions on how many qoutes the user wants
+// Here I want to access the final quote
+let finalQuote = `${quote1[quoteRandom1]} + ' ' + ${quote2[quoteRandom2]} + ' ' + ${quote3[quoteRandom3]}` + '.';
+// Console log the final quote 
+console.log(finalQuote);
 
-for (let i = 0; i < quote1.length + quote2.length + quote3.length; i++) {
-  var randomQuote = quote1 + ` ` + quote2 + ` ` + ` ` + quote3;
-  var singleQuote = [];
-  singleQuote.push(randomQuote);
+// Create a prompt for the users to enter the number of quotation they want
+
+function enterFunction() {
+  let numberOfquote = Number(prompt('Enter your number of quote you want to look at:'));
+  if (numberOfquote >= 1 && numberOfquote <= 5) {
+    console.log(finalQuote[1]);
+  }
+  for (let i = 1; i <= 5; i++) {
+    if (numberOfquote === 1 && numberOfquote <= 5) {
+      console.log(finalQuote);
+    } else {
+      console.log("choose number between 1 to 5");
+    }
+  }
+  return enterFunction();
 }
-console.log(singleQuote);
