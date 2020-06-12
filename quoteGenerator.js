b74347ca3d9fdec157565d94290ccfe4ca82e9d3
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-06-12 07:03:09 +0300

    finished working in the quote generating

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -56,75 +56,74 @@ function enterFunction() {
 
 // Let's create new arrays and they must be differnet from each other
 const loveQuotes = [
-[
-  "We love the things we love for what they are.",
-  "Do not settle for a relationship that won’t let you be yourself.",
-  "Only time can heal your broken heart, just as only time can heal his broken arms and legs."
-],
+  [
+    "We love the things we love for what they are.",
+    "Do not settle for a relationship that won’t let you be yourself.",
+    "Only time can heal your broken heart, just as only time can heal his broken arms and legs."
+  ],
 
-[
-  "Love is absolute loyalty.",
-  "People fade but loyalty never fades.",
-  "You can depend so much on certain people.",
-  "And that\’s love even if it doesn\’t seem very exciting."
-],
+  [
+    "Love is absolute loyalty.",
+    "People fade but loyalty never fades.",
+    "You can depend so much on certain people.",
+    "And that\’s love even if it doesn\’t seem very exciting."
+  ],
 
-[
-  "Love is a promise.",
-  "Love is a souvenir\,once given never forgotten.",
-  "Never let it disappear."
-]];
+  [
+    "Love is a promise.",
+    "Love is a souvenir\,once given never forgotten.",
+    "Never let it disappear."
+  ]];
 
 const inpirationalQuotes = [
-[  "Say yes more than no!",
-  "Why stop dreaming when you are awake?",
-  "The only time you should look back, is to see how far you’ve come."
-],
+  ["Say yes more than no!",
+    "Why stop dreaming when you are awake?",
+    "The only time you should look back, is to see how far you’ve come."
+  ],
 
-[
-  "Three things in life – your health, your mission, and the people you love.",
-  "That\’s it.",
-  "Happiness is the highest form of health."
-],
+  [
+    "Three things in life – your health, your mission, and the people you love.",
+    "That\’s it.",
+    "Happiness is the highest form of health."
+  ],
 
-[
-  "A good laugh and a long sleep are the best cures in the doctor\’s book.",
-  "The greatest of follies is to sacrifice health for any other kind of happiness.",
-  "What is called genius is the abundance of life and health."
-]];
+  [
+    "A good laugh and a long sleep are the best cures in the doctor\’s book.",
+    "The greatest of follies is to sacrifice health for any other kind of happiness.",
+    "What is called genius is the abundance of life and health."
+  ]];
 
-  const generateQuote = (quotesArray) => {
-    let randomIndex1 = Math.floor(Math.random() * quotesArray[0].length);
-    let randomIndex2 = Math.floor(Math.random() * quotesArray[1].length);
-    let randomIndex3 = Math.floor(Math.random() * quotesArray[2].length);
-    let string1 = quotesArray[0][randomIndex1];
-    let string2 = quotesArray[1][randomIndex2];
-    let string3 = quotesArray[2][randomIndex3];
-    const generatedQuote = `Generated quote : "${string1} ${string2} ${string3}"`;
-    return generatedQuote;
-  };
-  let continueProgram = 'yes';
-  while (continueProgram === 'yes') {
-    let numberOfQuotes = Number(prompt('How many quotes do you want to generate? (1min, 5max)'));
-    while (numberOfQuotes < 0 || numberOfQuotes > 5) {
-      numberOfQuotes = Number(prompt('How many quotes do you want to generate? (1min, 5max)'));
-    }
-    let typeOfQuote = prompt('Which set of quotes do you want to use? (love or inpirational)');
-    while (typeOfQuote !== 'love' && typeOfQuote !== 'inpirational') {
-      typeOfQuote = prompt('Which set of quotes do you want to use? (love or inpirational)');
-    }
-    let alertText = '';
-    if (typeOfQuote === 'love') {
-      for (let i = 0; i < numberOfQuotes; i++) {
-        alertText += '\n' + generateQuote(loveQuotes) + '\n';
-      }
-    } else {
-      for (let i = 0; i < numberOfQuotes; i++) {
-        alertText += '\n' + generateQuote(inpirationalQuotes) + '\n';
-      }
-    }
-    alert(alertText);
-    continueProgram = prompt('Do you want to run the program again? (yes, no)');
+const generateQuote = (quotesArray) => {
+  let randomIndex1 = Math.floor(Math.random() * quotesArray[0].length);
+  let randomIndex2 = Math.floor(Math.random() * quotesArray[1].length);
+  let randomIndex3 = Math.floor(Math.random() * quotesArray[2].length);
+  let string1 = quotesArray[0][randomIndex1];
+  let string2 = quotesArray[1][randomIndex2];
+  let string3 = quotesArray[2][randomIndex3];
+  const generatedQuote = `Generated quote : "${string1} ${string2} ${string3}"`;
+  return generatedQuote;
+};
+let continueProgram = 'yes';
+while (continueProgram === 'yes') {
+  let numberOfQuotes = Number(prompt('How many quotes do you want to generate? (1min, 5max)'));
+  while (numberOfQuotes < 0 || numberOfQuotes > 5) {
+    numberOfQuotes = Number(prompt('How many quotes do you want to generate? (1min, 5max)'));
   }
-  alert('Thank you for using my quote generator');
-  
+  let typeOfQuote = prompt('Which set of quotes do you want to use? (love or inpirational)');
+  while (typeOfQuote !== 'love' && typeOfQuote !== 'inpirational') {
+    typeOfQuote = prompt('Which set of quotes do you want to use? (love or inpirational)');
+  }
+  let alertText = '';
+  if (typeOfQuote === 'love') {
+    for (let i = 0; i < numberOfQuotes; i++) {
+      alertText += '\n' + generateQuote(loveQuotes) + '\n';
+    }
+  } else {
+    for (let i = 0; i < numberOfQuotes; i++) {
+      alertText += '\n' + generateQuote(inpirationalQuotes) + '\n';
+    }
+  }
+  alert(alertText);
+  continueProgram = prompt('Do you want to run the program again? (yes, no)');
+}
+alert('Thank you for using my quote generator');
